77ef95ba7d4944a3856fc14aab7e9aa03740a7a3
asuetomi <a_sue@tka.att.ne.jp>
2018-12-02 00:39:16 +0900

    詳細が開業されないけどとりあえず動く状態

--- app.py
+++ app.py
@@ -143,5 +143,5 @@ def search():
 	return response
 
 if __name__ == "__main__":
-    print(linebreaksbr('aaa\nbbb'))
+    # print(linebreaksbr('aaa\nbbb'))
     app.run(debug=False, host='0.0.0.0', port=8080)
